a716c4cc688767d0a011d387eea76a987b9f2e9f
Steve Warren <steve-warren@users.noreply.github.com>
2024-09-30 20:57:40 -0500

    issue #7 - add binary primitives helper class.

---
+++ Buffers/BinaryPrimitives.cs
@@ -0,0 +1,28 @@
+namespace LunaDB.Buffers;
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+public static class BinaryPrimitives
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void EnsureLittleEndianness()
+    {
+        if (BitConverter.IsLittleEndian is false)
+            throw new NotSupportedException("Big Endian byte order not supported.");
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void WriteInt16(Span<byte> destination, short value) =>
+        MemoryMarshal.Write<short>(destination, in value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void WriteInt32(Span<byte> destination, int value) =>
+        MemoryMarshal.Write<int>(destination, in value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static short ReadInt16(Span<byte> source) => MemoryMarshal.Read<short>(source);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int ReadInt32(Span<byte> source) => MemoryMarshal.Read<int>(source);
+}
